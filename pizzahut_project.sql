dd99c80a7022b0c18c71904c82ace26bc6c17582
Sanika dalvi <68457070+Sanika-dalvi-18@users.noreply.github.com>
2024-10-01 12:39:20 +0530

    Update pizzahut_project.sql

--- pizzahut_project.sql
+++ pizzahut_project.sql
@@ -1,18 +1,5 @@
-create database pizzahut;
-use pizzahut;
-create table orders (
-order_id int primary key not null,
-order_date date not null,
-order_time time not null);
-
-create table order_details (
-order_details_id int not null,
-order_id int not null,
-pizza_id text not null,
-quantity int not null,
-primary key(order_details_id)
-);
-select * from order_details;
+-- Pizzahut Sales Data Analysis using SQL
+-- Solutions of 13 business problems
 
 
 -- 1)Retrieve the total number of orders placed.
